0278db15ebc6f2c93f8e4f5167f06fe43ac6564e
FrontHubYoutube <tapeshko4@gmail.com>
2024-11-21 22:46:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const navbar = document.querySelector('.navbar');
-const cartItem = document.querySelector('.cart-items-container');
-const searchForm = document.querySelector('.search-form');
-
-
-document.querySelector('#menu-btn').onclick = () => {
-	navbar.classList.toggle('active');
-	searchForm.classList.remove('active');
-	cartItem.classList.remove('active');
-}
-document.querySelector('#cart-btn').onclick = () => {
-	cartItem.classList.toggle('active');
-	navbar.classList.remove('active');
-	searchForm.classList.remove('active');
-}
-document.querySelector('#search-btn').onclick = () => {
-	searchForm.classList.toggle('active');
-	cartItem.classList.remove('active');
-	navbar.classList.remove('active');
-}
+const navbar = document.querySelector(".navbar");
+const cartItem = document.querySelector(".cart-items-container");
+const searchForm = document.querySelector(".search-form");
+const shadow = document.querySelectorAll(".shadow");
+const menu = document.querySelector("#menu-btn");
+menu.onclick = () => {
+  menu.classList.toggle("fa-x");
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+document.querySelector("#cart-btn").onclick = () => {
+  menu.classList.remove("fa-x");
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+document.querySelector("#search-btn").onclick = () => {
+  menu.classList.remove("fa-x");
+  searchForm.classList.toggle("active");
+  cartItem.classList.remove("active");
+  navbar.classList.remove("active");
+};
 window.onscroll = () => {
-	navbar.classList.remove('active');
-	searchForm.classList.remove('active');
-	cartItem.classList.remove('active');
-}
+  menu.classList.remove("fa-x");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+shadow.forEach((item) => {
+  item.onclick = () => {
+    menu.classList.remove("fa-x");
+    navbar.classList.remove("active");
+    cartItem.classList.remove("active");
+  };
+});
